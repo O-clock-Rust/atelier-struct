c068fa0a0cf5b41a73b3899bb4a4a78b44444a73
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-08-27 12:42:23 +0200

    3. struct : constructor

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ struct Car {
     year: u32,
 }
 
+impl Car {
+    fn new(brand: String, model: String, year: u32) -> Self {
+        Car { brand, model, year }
+    }
+}
+
 fn main() {
     let current_year: u32 = 2024;
     println!("This year is {}", current_year);
