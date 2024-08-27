5a8395d0707c6cee619d7ecab90a02a9e4bc8c59
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-08-27 12:42:49 +0200

    3. struct : implement describe

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ impl Car {
     fn new(brand: String, model: String, year: u32) -> Self {
         Car { brand, model, year }
     }
+
+    fn describe(&self) -> String {
+        format!("In {}, {} created the {}", self.year, self.brand, self.model)
+    }
 }
 
 fn main() {
